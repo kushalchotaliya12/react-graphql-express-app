f2a54906532a1a4318c76686bf47bf5a98a96b2f
Kushal Chotaliya <kushalv.1997@gmail.com>
2021-01-27 00:11:46 -0500

    Added Functionality

--- src/PostViewer.js
+++ src/PostViewer.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import { Table } from 'reactstrap';
+import React from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { Table } from "reactstrap";
 
 export const GET_POSTS = gql`
   query GetPosts {
@@ -13,25 +13,41 @@ export const GET_POSTS = gql`
   }
 `;
 
-export default () => (
+const rowStyles = (post, canEdit) =>
+  canEdit(post) ? { cursor: "pointer", fontWeight: "bold" } : {};
+
+const PostViewer = ({ canEdit, onEdit }) => (
   <Query query={GET_POSTS}>
-    {({ loading, data }) => !loading && (
-      <Table>
-        <thead>
-          <tr>
-            <th>Author</th>
-            <th>Body</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.posts.map(post => (
-            <tr key={post.id}>
-              <td>{post.author}</td>
-              <td>{post.body}</td>
+    {({ loading, data }) =>
+      !loading && (
+        <Table>
+          <thead>
+            <tr>
+              <th>Author</th>
+              <th>Body</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-    )}
+          </thead>
+          <tbody>
+            {data.posts.map((post) => (
+              <tr
+                key={post.id}
+                style={rowStyles(post, canEdit)}
+                onClick={() => canEdit(post) && onEdit(post)}
+              >
+                <td>{post.author}</td>
+                <td>{post.body}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )
+    }
   </Query>
 );
+
+PostViewer.defaultProps = {
+  canEdit: () => false,
+  onEdit: () => null,
+};
+
+export default PostViewer;
